848115b26f1bf21c3feeb9b6839d4799a41f0bc9
ftrole <50705762+ftrole@users.noreply.github.com>
2020-05-28 20:03:50 +0200

    aggiunto insert, iteratori, costruttore di default

--- dList.h
+++ dList.h
@@ -1,6 +1,6 @@
 #ifndef DLIST_H
 #define DLIST_H
-#include<iostream>
+#include <iostream>
 using std::cout;
 using std::endl;
 
@@ -13,8 +13,8 @@ private:
     public:
         T info;
         nodo *prev, *next;
-        nodo(const T &t = T(), nodo *p = nullptr, nodo *n = nullptr); //costruttore di default di nodo
-        ~nodo(); //chiamata ricorsiva a distruttore di nodo (sennò la delete su dList first dealloca solo il primo nodo della lista)
+        nodo(const T &t = T(), nodo *p = nullptr, nodo *n = nullptr);
+        ~nodo();
     };
 
     nodo *last, *first; // lista vuota iff first==last==nullptr    per garantire inserimento in testa e coda in tempo costante
@@ -25,21 +25,50 @@ public:
     ~dList();
     dList(const dList &d);
     dList(unsigned int k, const T &t);
+    dList();
     dList &operator=(const dList &d);
     void insertFront(const T &t);
     void insertBack(const T &t);
+    void popFront();
+    void popBack();
     bool operator<(const dList &d) const;
+
+    class iterator
+    {
+        friend class dList<T>;
+        friend class const_iterator; //per accederere a p e pte
+
+    private:
+        nodo *ptr;
+        bool pastTheEnd;                     // true iff iterator e' "past-the-end"
+        iterator(nodo *p, bool pte = false); // conversione nodo* => iterator
+
+    public:
+        iterator();
+        T &operator*() const;
+        T *operator->() const;
+        iterator &operator++();
+        iterator &operator--();
+        bool operator==(const iterator &x) const;
+        bool operator!=(const iterator &x) const;
+    };
+    iterator begin() const;
+    iterator end() const;
+    iterator insert(iterator &i, const T &t);
+    iterator remove(iterator &i); 
 
     class const_iterator
     {
         friend class dList<T>;
+
     private:
         nodo *ptr;
-        bool pastTheEnd; // true iff constiterator e' "past-the-end"
-        const_iterator(nodo *p, bool pte = false);// conversione nodo* => constiterator
+        bool pastTheEnd;                           // true iff constiterator e' "past-the-end"
+        const_iterator(nodo *p, bool pte = false); // conversione nodo* => const_iterator
 
     public:
         const_iterator();
+        const_iterator(iterator &i); //conversione iterator->const_iterator
         const T &operator*() const;
         const T *operator->() const;
         const_iterator &operator++();
@@ -47,35 +76,31 @@ public:
         bool operator==(const const_iterator &x) const;
         bool operator!=(const const_iterator &x) const;
     };
-    const_iterator begin() const;
-    const_iterator end() const;
+    const_iterator cbegin() const;
+    const_iterator cend() const;
 };
-
-
 
 /*********************************************IMPLEMENTAZIONE************************************************/
 
-
-
 /**********************************NODO**********************************/
 
-template<class T>
+template <class T>
 dList<T>::nodo::nodo(const T &t, dList<T>::nodo *p, dList::nodo *n) : info(t), prev(p), next(n) {}
 
-template<class T>
+template <class T>
 dList<T>::nodo::~nodo() { delete next; }
 
 /**********************************DLIST**********************************/
 
-template<class T>
-typename dList<T>::nodo * dList<T>::copy(nodo *p, nodo *&l) //si passa last per riferimento perché ancora non si sa quale sia l'ultimo nodo (il primo si)
+template <class T>
+typename dList<T>::nodo *dList<T>::copy(nodo *p, nodo *&l) //si passa last per riferimento perché ancora non si sa quale sia l'ultimo nodo (il primo si)
 {
     if (p == nullptr) //lista da copiare vuota
         return l = nullptr;
     nodo *prec = new nodo(p->info, nullptr, nullptr); //lista da copiare ha almeno un nodo
-    nodo *succ = prec;    //da inizializzare qui per l=succ
+    nodo *succ = prec;                                //da inizializzare qui per l=succ
     nodo *start = prec;
-    while (p->next != nullptr)  // lista da copiare ha almeno 2 nodi
+    while (p->next != nullptr) // lista da copiare ha almeno 2 nodi
     {
         succ = new nodo(p->next->info, prec, nullptr);
         prec->next = succ;
@@ -86,7 +111,7 @@ typename dList<T>::nodo * dList<T>::copy(nodo *p, nodo *&l) //si passa last per 
     return start;
 }
 
-template<class T>
+template <class T>
 bool dList<T>::isLess(nodo *d1, nodo *d2)
 {
     if (d2 == nullptr)
@@ -96,17 +121,17 @@ bool dList<T>::isLess(nodo *d1, nodo *d2)
         return true;
     // d1 e d2 NON vuote
     return d1->info < d2->info ||
-            (d1->info == d2->info && isLess(d1->next, d2->next));
+           (d1->info == d2->info && isLess(d1->next, d2->next));
 }
 
-template<class T>
-dList<T>::dList (const dList<T> &d) : first(copy(d.first, last)) // first è ritornato dalla funzione, last viene modificato da copy
-{                                                  //grazie all'aliasing
+template <class T>
+dList<T>::dList(const dList<T> &d) : first(copy(d.first, last)) // first è ritornato dalla funzione, last viene modificato da copy
+{                                                               //grazie all'aliasing
     // first=copy(d.first,last);
 }
 
-template<class T>
-dList<T>& dList<T>::operator=(const dList<T> &d)
+template <class T>
+dList<T> &dList<T>::operator=(const dList<T> &d)
 {
     if (this != &d)
     {
@@ -116,7 +141,7 @@ dList<T>& dList<T>::operator=(const dList<T> &d)
     return *this;
 }
 
-template<class T>
+template <class T>
 void dList<T>::insertFront(const T &t)
 {
     first = new nodo(t, nullptr, first);
@@ -132,7 +157,7 @@ void dList<T>::insertFront(const T &t)
     }
 }
 
-template<class T>
+template <class T>
 void dList<T>::insertBack(const T &t)
 {
     last = new nodo(t, last, nullptr);
@@ -148,14 +173,47 @@ void dList<T>::insertBack(const T &t)
     }
 }
 
-template<class T>
+template <class T>
+typename dList<T>::iterator dList<T>::insert(iterator &i, const T &t) //inseruisce t prima del nodo puntato da i e ritorna l'iteratore che punta al nuovo nodo
+{
+    if (i.ptr == nullptr) //la lista è vuota
+    {
+        insertFront(t);
+        return first; //conversione implicita in iterator
+    }
+
+    else
+    {
+        if (i == end()) //iteratore past the end
+            insertBack(t);
+        else if (i == begin()) //iteratore primo elemento
+            insertFront(t);
+        else //caso generale
+        {
+            nodo *temp = new nodo(t, i.ptr->prev, i.ptr);
+            (i.ptr->prev)->next = temp;
+            i.ptr->prev = temp;
+        }
+        return --i;
+    }
+}
+
+template <class T>
 dList<T>::dList(unsigned int k, const T &t) : last(nullptr), first(nullptr)
 {
     for (unsigned int i = 0; i < k; ++i)
         insertFront(t);
 }
+template <class T>
+dList<T>::dList() : last(nullptr), first(nullptr) {}
 
-template<class T>
+template <class T>
+dList<T>::~dList()
+{
+    delete first;
+}
+
+template <class T>
 bool dList<T>::operator<(const dList<T> &d) const
 {
     return isLess(first, d.first);
@@ -163,39 +221,42 @@ bool dList<T>::operator<(const dList<T> &d) const
 
 /**********************************CONST_ITERATOR**********************************/
 
-template<class T>
-typename dList<T>::const_iterator dList<T>::begin() const
+template <class T>
+typename dList<T>::const_iterator dList<T>::cbegin() const
 {
     return first;
 }
 
-template<class T>
-typename dList<T>::const_iterator dList<T>::end() const
+template <class T>
+typename dList<T>::const_iterator dList<T>::cend() const
 {
     if (last == nullptr) //dList vuota ritorna 0
         return nullptr;
     return const_iterator(last + 1, true); // dList ha almeno un nodo ritorna iterator pte
 }
 
-template<class T>
+template <class T>
 dList<T>::const_iterator::const_iterator(dList<T>::nodo *p, bool pte) : ptr(p), pastTheEnd(pte) {}
 
-template<class T>
+template <class T>
 dList<T>::const_iterator::const_iterator() : ptr(nullptr), pastTheEnd(false) {}
 
-template<class T>
+template <class T>
+dList<T>::const_iterator::const_iterator(iterator &i) : ptr(i.ptr), pastTheEnd(i.pte) {}
+
+template <class T>
 const T &dList<T>::const_iterator::operator*() const
 {
     return ptr->info;
 }
 
-template<class T>
+template <class T>
 const T *dList<T>::const_iterator::operator->() const
 {
     return &(ptr->info);
 }
 
-template<class T>
+template <class T>
 typename dList<T>::const_iterator &dList<T>::const_iterator::operator++()
 {
     if (ptr != nullptr)
@@ -204,7 +265,7 @@ typename dList<T>::const_iterator &dList<T>::const_iterator::operator++()
         {
             if (ptr->next == nullptr)
             {
-                ++ptr;   //chiamata al ++ prefisso sugli interi: ptr è un indirizzo
+                ++ptr; //chiamata al ++ prefisso sugli interi: ptr è un indirizzo
                 pastTheEnd = true;
             }
             else
@@ -216,7 +277,7 @@ typename dList<T>::const_iterator &dList<T>::const_iterator::operator++()
     return *this;
 }
 
-template<class T>
+template <class T>
 typename dList<T>::const_iterator &dList<T>::const_iterator::operator--()
 {
     if (ptr != nullptr)
@@ -232,20 +293,111 @@ typename dList<T>::const_iterator &dList<T>::const_iterator::operator--()
     return *this;
 }
 
-template<class T>
+template <class T>
 bool dList<T>::const_iterator::operator==(const dList<T>::const_iterator &x) const
 {
     return ptr == x.ptr;
 }
 
-template<class T>
+template <class T>
 bool dList<T>::const_iterator::operator!=(const dList<T>::const_iterator &x) const
 {
     return ptr != x.ptr;
 }
 
+/**********************************ITERATOR**********************************/
 
+template <class T>
+typename dList<T>::iterator dList<T>::begin() const
+{
+    return first;
+}
 
+template <class T>
+typename dList<T>::iterator dList<T>::end() const
+{
+    if (last == nullptr) //dList vuota ritorna 0
+        return nullptr;
+    return iterator(last + 1, true); // dList ha almeno un nodo ritorna iterator pte
+}
 
+template <class T>
+dList<T>::iterator::iterator(dList<T>::nodo *p, bool pte) : ptr(p), pastTheEnd(pte) {}
+
+template <class T>
+dList<T>::iterator::iterator() : ptr(nullptr), pastTheEnd(false) {}
+
+template <class T>
+T &dList<T>::iterator::operator*() const
+{
+    return ptr->info;
+}
+
+template <class T>
+T *dList<T>::iterator::operator->() const
+{
+    return &(ptr->info);
+}
+
+template <class T>
+typename dList<T>::iterator &dList<T>::iterator::operator++()
+{
+    if (ptr != nullptr)
+    {
+        if (!pastTheEnd)
+        {
+            if (ptr->next == nullptr)
+            {
+                ++ptr; //chiamata al ++ prefisso sugli interi: ptr è un indirizzo
+                pastTheEnd = true;
+            }
+            else
+            {
+                ptr = ptr->next;
+            }
+        }
+    }
+    return *this;
+}
+
+template <class T>
+typename dList<T>::iterator &dList<T>::iterator::operator--()
+{
+    if (ptr != nullptr)
+    {
+        if (pastTheEnd)
+        {
+            --ptr;
+            pastTheEnd = false;
+        }
+        else
+            ptr = ptr->prev;
+    }
+    return *this;
+}
+
+template <class T>
+bool dList<T>::iterator::operator==(const dList<T>::iterator &x) const
+{
+    return ptr == x.ptr;
+}
+
+template <class T>
+bool dList<T>::iterator::operator!=(const dList<T>::iterator &x) const
+{
+    return ptr != x.ptr;
+}
 
 #endif // DLIST_H
+
+/* 
+DONE:
+-iteratori
+-inserimento dato l'iteratore
+TODO:
+- pop front e pop back
+-eliminazione ad iteratore
+-eccezioni all'inserimento e modifica?
+-input e output?
+questi due non presenti in stl::list
+ */
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "dList.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    dList<int> lista;
+    dList<int>::iterator i = lista.begin();
+    auto j=lista.insert(i, 3);
+    cout << *(j) << endl;
+    dList<int>::iterator l = lista.end();
+    auto m=lista.insert(l, 6);
+    for (dList<int>::const_iterator c = lista.cbegin(); c != lista.cend(); ++c)
+        std::cout << *c << ' ';
+    std::cout << std::endl;
 }
