f2d601886ad17cddcd05c964ce85093e34f1ef85
ftrole <50705762+ftrole@users.noreply.github.com>
2020-06-01 00:12:50 +0200

    aggiunto isLogged e gtNome
    servivano per i metodi della lista

--- inquilino.cpp
+++ inquilino.cpp
@@ -3,33 +3,45 @@
 Inquilino::Inquilino(const string& user, const string& pw) : _user(user), _password(pw), _creditoDebito(0), _punteggio(0) {}
 
 void Inquilino::modifica(const string& user, const string& pw)
+void Inquilino::modifica(string user, string pw)
 {
     _user = user;
     _password = pw;
 }
 void Inquilino::setCD(const double& cd)
+void Inquilino::setCD(double cd)
 {
     _creditoDebito = _creditoDebito + cd;
 }
 void Inquilino::setPunteggio(const unsigned short int & punti)
+void Inquilino::setPunteggio(short int punti)
 {
     _punteggio += punti;
+    _punteggio = _punteggio + punti;
     if (_punteggio > _puntiMax)
         _punteggio = _puntiMax;
     else if (_punteggio < _puntiMin)
         _punteggio = _puntiMin;
 }
 unsigned short int Inquilino::isAdmin() const
+unsigned short Inquilino::isAdmin()
 {
     return 0;
 }
 bool Inquilino::puoPosporre() const
+bool Inquilino::puoPosporre()
 {
     return _punteggio >= _sogliaPosponi;
+    if (_punteggio >= _sogliaPosponi)
+        return true;
+    return false;
 }
 
 double Inquilino::getCreditoDebito() const{
     return _creditoDebito;
+std::string Inquilino::getNome() const
+{
+    return _user;
 }
 
 unsigned short int Inquilino::getPunteggio() const{
@@ -38,4 +50,6 @@ unsigned short int Inquilino::getPunteggio() const{
 
 unsigned short int Inquilino::_puntiMin = 0;
 unsigned short int Inquilino::_puntiMax = 100;
-unsigned short int Inquilino::_sogliaPosponi = 40;
+unsigned short int Inquilino::_sogliaPosponi = 40;short int Inquilino::_puntiMin = 0;
+short int Inquilino::_puntiMax = 0;
+short int Inquilino::_sogliaPosponi = 0;
--- inquilino.h
+++ inquilino.h
@@ -15,6 +15,9 @@ private:
     static unsigned short int _puntiMin;
     static unsigned short int _puntiMax;
     static unsigned short int _sogliaPosponi;
+    static short int _puntiMin;
+    static short int _puntiMax;
+    static short int _sogliaPosponi;
 
 public:
     Inquilino(const string&, const string&);        //utente da nome e pw
@@ -26,6 +29,13 @@ public:
     double getCreditoDebito() const;
     unsigned short int getPunteggio() const;
 
+    void modifica(string, string); //modifica username e/o password
+    void setCD(double);            //modifica il credito/debito aggiungendo (o togliendo se negativo)
+    void setPunteggio(short int);  //modifica il punteggio aggiungendo (o togliendo se negativo)
+    virtual unsigned short isAdmin();   //ritorna un intero che indica la categoria dell'utente (0 per Inquilino)
+    virtual bool puoPosporre(); //bool indica se l'inquilino può o no posporre
+    string getNome() const;
+    bool isLogged(string &, string &) const;
 };
 
-#endif
+#endif#endif
